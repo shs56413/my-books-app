b810c2e51793426af92f821d0862c8a9fe86f8d6
Sou Saeteurn <Souhdesigns@gmail.com>
2022-10-26 17:10:39 -0700

    added and styled side bar

---
+++ src/components/SideBar.js
@@ -0,0 +1,12 @@
+const SideBar = () => {
+  return (
+    <div className="side-bar">
+      <div className="side-info">
+        <h1>Home</h1>
+        <h2></h2>
+      </div>
+    </div>
+  )
+}
+
+export default SideBar
