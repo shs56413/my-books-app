3b1581d4905863e19a05c7c341353cbca75ec49b
Sou Saeteurn <Souhdesigns@gmail.com>
2022-11-05 17:20:58 -0700

    add quote generator

--- src/App.js
+++ src/App.js
@@ -2,29 +2,64 @@ import './App.css'
 import NavBar from './components/NavBar'
 import data from './data/data.json'
 import Book from './components/Book'
-import SearchBar from './components/SearchBar'
 import Banner from './components/Banner'
+import Quote from './components/Quote'
+import React, { useState, useEffect} from 'react';
 
 function App(props) {
+  const [quote, setQuote]= useState({
+    anime: null,
+    character: null,
+    quote:null
+  });
+
+  
+
+  const fetchQuote = async() => {
+    return await fetch('https://animechan.vercel.app/api/random')
+    .then(response => response.json());
+  }
+
+  const generate = async () => {
+    setQuote(await fetchQuote());
+  }
+
+    useEffect(async () =>{
+      setQuote(await fetchQuote());
+    },[]);
+  
+
   return (
     <div className="App">
 
 <header className="App-header">
         <NavBar />
-        <SearchBar />
-        <div className="title">
-          <h1>Bookster</h1>
-        </div>
+        <h1>Bookster</h1>
+
+        
+      
       </header>
+
       <main>
         {data.books.map((book) => (
           <Book key={book.id} book={book} />
         ))}
       </main>
+       <footer className="quote-box"> 
+       <h1>Anime Quotes</h1>
+      <Quote quote={quote} />
+        <button onClick={generate}>Generate new quote</button>
+</footer>
       <Banner />
 
     </div>
+    
+
+
+    
   )
 }
+
+
 
 export default App
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,6 @@
 const Banner = (props) => {
   return (
-    <div className="nav-bar">
+    <div className="banner">
       <img src="https://upload.wikimedia.org/wikipedia/commons/5/52/Sci-Fi_Channel_1999_logo.png"></img>
       <img src="https://upload.wikimedia.org/wikipedia/commons/5/52/Sci-Fi_Channel_1999_logo.png"></img>
       <img src="https://upload.wikimedia.org/wikipedia/commons/5/52/Sci-Fi_Channel_1999_logo.png"></img>
